9e20cde345c703276f2170a8473eff9daaec081e
Cook1619 <cook6348@gmail.com>
2018-06-13 16:50:11 -0500

    State is working with no errors

--- first-app/src/components/App.jsx
+++ first-app/src/components/App.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  
+    constructor(props){
+        super(props);
+        this.state = {text: "Villian"}
+    }
     render() {
         return (
             <h1>{this.props.superhero.name}</h1>
