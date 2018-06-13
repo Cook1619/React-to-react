55026c3f495e711de198693650e31d1133fd5f69
Cook1619 <cook6348@gmail.com>
2018-06-13 16:53:37 -0500

    State and props working together

--- first-app/src/components/App.jsx
+++ first-app/src/components/App.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {text: "Villian"}
+        this.state = {text: "VS Superman!!"}
     }
     render() {
         return (
-            <h1>{this.props.superhero.name}</h1>
+            <h1>{this.props.superhero.name} {this.state.text}</h1>
         )
     }
 
