fcbe765280974163a79652e194ca376684c5c479
Cook1619 <cook6348@gmail.com>
2018-06-14 13:27:53 -0500

    My PH is working along with my onChange event

--- first-app/src/components/App.jsx
+++ first-app/src/components/App.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {text: "VS Superman!!"}
+        this.state = {
+            text: "VS Superman!!"
+        }
     }
     render() {
         return (
-            <h1>{this.props.superhero.name} {this.state.text}</h1>
+            <React.Fragment>
+                <h1>{this.props.superhero.name} {this.state.text}</h1>
+                <input placeholder={this.state.text}
+                       onChange={(event) => {
+                        this.setState({ text: event.target.value })
+                    }}></input>
+
+            </React.Fragment>
+
         )
     }
 
