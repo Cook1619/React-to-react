e141491322bc9acab9297c7cf0f00711756f9e0a
Cook1619 <cook6348@gmail.com>
2018-06-15 11:07:36 -0500

    Maybe working?

--- first-app/src/components/App.jsx
+++ first-app/src/components/App.jsx
@@ -11,22 +11,29 @@ class App extends Component {
 
     }
     hasLoaded = () => {
-        this.setState({hasLoaded: !this.state.hasLoaded});
+        this.setState({ hasLoaded: !this.state.hasLoaded });
     }
     render() {
-        return (
-            <React.Fragment>
-                <h1>{this.props.superhero.name} {this.state.text}</h1>
-                <input
-                    placeholder={this.state.text}
-                    onChange={(event) => {
-                        this.setState({ text: event.target.value })
-                    }}></input>
-                <button onClick={this.hasLoaded}>Click me</button>
-            </React.Fragment>
-
-        )
+        if (this.state.hasLoaded) {
+            return (
+                <React.Fragment>
+                    <h1>{this.props.superhero.name} {this.state.text}</h1>
+                    <input
+                        placeholder={this.state.text}
+                        onChange={(event) => {
+                            this.setState({ text: event.target.value })
+                        }}></input>
+                    <button onClick={this.hasLoaded}>Click me</button>
+                </React.Fragment>
+            )
+        } else {
+            return (
+                <React.Fragment>
+                    <h1>LOADING.....</h1>
+                    <button onCLick={this.hasLoaded}>Click</button>
+                </React.Fragment>
+            )
+        }
     }
-
 }
 export default App;
